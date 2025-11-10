7d85138f44be35f9afae68b81ff9b7e34527d889
Franklin Hyriol <franklin.razafinandrasana@pulse.mg>
2025-11-10 11:19:56 +0300

    add seo optimisation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,33 @@ import Footer from "../components/Footer/Footer";
 export const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.eyechallenge.fun"),
   title: "EyeChallenge - Online Vision & Color Perception Tests",
   description:
     "EyeChallenge is an online platform for vision and color perception tests. It provides a range of tests to assess visual acuity, color vision, and other aspects of visual perception.",
+  alternates: {
+    canonical: "https://www.eyechallenge.fun",
+    languages: {
+      "en-US": "https://www.eyechallenge.fun",
+    },
+  },
+  openGraph: {
+    title: "EyeChallenge - Online Vision & Color Perception Tests",
+    description:
+      "Test your vision and color perception with our online challenges.",
+    url: "https://www.eyechallenge.fun",
+    siteName: "EyeChallenge",
+    images: [
+      {
+        url: "https://www.eyechallenge.fun/hero_image.svg",
+        width: 800,
+        height: 600,
+        alt: "EyeChallenge Hero Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
